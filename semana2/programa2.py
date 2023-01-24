a60f8ac1e0e3985315f78d91abd693aeedbf793f
JairVaz13 <122801418+JairVaz13@users.noreply.github.com>
2023-01-24 14:04:57 +0000

    se creó un programa para almacenar variables, también para sumar variables y separa variables .

---
+++ semana2/programa2.py
@@ -0,0 +1,10 @@
+"""
+programa1
+nombre: jair vazquez cordero
+fecha: 24/01/2023
+descripcion:caracteres 
+"""
+variable1 = "hola" # variable  para almacenar una cadena de caracteres 
+variable2 = 619 # variable  para almacenar una cadena de caracteres 
+print(variable1 + variable2) # para la suma de dos variables 
+print("{} {}". format(variable1, variable2)) # funcion para poder tener espacios entre dos variables con la funcion "format"
